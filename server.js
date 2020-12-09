c1f7f9210bb9b5dd23046578513b3dd2b02801ab
Candy Garcia <candygarcia014@gmail.com>
2020-12-09 09:45:06 -0800

    added server.js

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const path = require("path")
+const fs = require("fs");
+const { Recoverable } = require("repl");
+const app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//configuring app 
+app.use(express.static("public"))
+const dbData = JSON.parse(fs.readFileSync("db/db.json", "utf8"))
+let currentId = 2
+
+//Data/API routes
+app.get("/api/notes", function(req, res) {
+    res.send(dbData)
+});
+
+app.post("/api/notes", function(req, res) {
+    dbData.push({
+        ...req.body,
+        id: currentId
+    })
+    currentId++
+    fs.writeFileSync("db/db.json", JSON.stringify(dbData), "utf8")
+    res.send(dbData)
+});
+
+// app.delete("/api/notes/:id", function(req, res) {
+//     res.sendFile(path.join(__dirname, "public/notes.html"));
+// });
+
+//HTML Routes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+//* must be the last route
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
+
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
